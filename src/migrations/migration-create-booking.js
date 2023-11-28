9532bd6ad74b687560da17243ffec5cdb98400d5
MG Apocalypse <mgapocalypse01032023@gmail.com>
2023-11-28 18:54:50 +0700

    Add new my project

---
+++ src/migrations/migration-create-booking.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('bookings', {
+            // statusID: DataTypes.STRING,
+            // doctorID: DataTypes.INTEGER,
+            // patientID: DataTypes.INTEGER,
+            // date: DataTypes.DATE,
+            // timeType: DataTypes.STRING,
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            statusID: {
+                type: Sequelize.STRING
+            },
+            doctorID: {
+                type: Sequelize.INTEGER
+            },
+            patientID: {
+                type: Sequelize.INTEGER
+            },
+            date: {
+                type: Sequelize.DATE
+            },
+            timeType: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('bookings');
+    }
+};
---
+++ src/migrations/migration-create-schedule.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('schedules', {
+            // currentNumber: DataTypes.INTEGER,
+            // maxNumber: DataTypes.INTEGER,
+            // data: DataTypes.DATA,
+            // timeType: DataTypes.STRING,
+            // doctocId: DataTypes.INTEGER,
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            currentNumber: {
+                type: Sequelize.INTEGER
+            },
+            maxNumber: {
+                type: Sequelize.INTEGER
+            },
+            date: {
+                type: Sequelize.DATE
+            },
+            timeType: {
+                type: Sequelize.STRING
+            },
+            doctocId: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('schedules');
+    }
+};
